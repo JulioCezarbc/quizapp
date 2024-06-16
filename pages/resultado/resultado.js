41830b50702f6c43982cebaf6d002831474f15a4
Júlio Cordeiro <julio04.cordeiro@gmail.com>
2024-06-16 13:10:49 +0000

    adicionado o javascript para a pagina de resultados

---
+++ pages/resultado/resultado.js
@@ -0,0 +1,11 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
+
+const body = document.querySelector("body");
+const botaoTema = document.querySelector(".tema button");
+
+botaoTema.addEventListener("click", () =>{
+    trocarTema(body,botaoTema);
+
+});
+
+verificarTema(body, botaoTema);
